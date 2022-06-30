ab0a7c1b22c6520f4f22b4a0cd9a4083d59858f4
mollikacomputer <mollikacomputer3@gmail.com>
2022-07-01 01:43:07 +0600

    show display data get from server data

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState, useSyncExternalStore } from 'react';
 import {Link} from 'react-router-dom';
 const Home = () => {
+    // show data from server
+    const [users, setUsers] = useState([]);
+    useEffect(()=>{
+        fetch(`http://localhost:5000/user`)
+        .then(res => res.json())
+        .then(data => setUsers(data) )
+    }, []);
     return (
         <div>
             <h2> Home page</h2>
@@ -8,6 +15,12 @@ const Home = () => {
                 <li>
                     <Link to="/adduser" > Add User </Link>
                 </li>
+            </ul>
+            <h2> Total Users ${users.length} </h2>
+            <ul>
+                {
+                    users.map(u => <li> {u.name} : {u.email} </li> )
+                }
             </ul>
         </div>
     );
